2716de6672037bb98f676ca9eff5fe4313ad5d24
Zachary Roy <vgd199@gmail.com>
2024-07-15 15:52:47 -0700

    added font awesome, added a navbar that has the items for listening and socials

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,114 @@
-const Header = () => {
-  return (
-    <>
-      <div className="absolute font-arial z-50 flex justify-between px-4 mt-3 w-full">
-        <h1 className="text-5xl">Pacific Palace</h1>
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faFacebook,
+  faInstagram,
+  faItunesNote,
+  faSpotify,
+  faTiktok,
+} from "@fortawesome/free-brands-svg-icons"
+import { useState } from "react"
 
-        <nav className="text-2xl">
-          <a href="#tour" className="mx-3">
-            Tour
-          </a>
-          <a href="#videos">Videos</a>
-        </nav>
-      </div>
-    </>
+const Header = () => {
+  const [isSocialHovered, setIsSocialHovered] = useState(false)
+  const [isListenHovered, setIsListenHovered] = useState(false)
+
+  return (
+    <div className="absolute font-arial z-50 flex justify-between px-4 mt-3 w-full">
+      <h1 className="text-5xl select-none">Pacific Palace</h1>
+
+      <nav className="text-2xl flex items-center">
+        <a href="#tour" className="mx-3 link-hover">
+          Tour
+        </a>
+        <a className="mx-3 link-hover" href="#videos">
+          Videos
+        </a>
+
+        <div
+          className="flex items-center mx-3 relative"
+          onMouseEnter={() => setIsSocialHovered(true)}
+          onMouseLeave={() => setIsSocialHovered(false)}
+        >
+          <a className="cursor-pointer">Socials</a>
+
+          <div
+            className={`absolute flex flex-col top-10 left-6 social-links ${
+              isSocialHovered ? "opacity-100" : "opacity-0"
+            }`}
+          >
+            <a
+              className={`my-2 ${
+                isSocialHovered ? "opacity-100" : "opacity-0"
+              }`}
+              href="https://www.instagram.com/pacific.palaceee/"
+            >
+              <FontAwesomeIcon
+                className="ease-in-out duration-100 mx-3 text-2xl hover:text-gray-900"
+                icon={faInstagram}
+              />
+            </a>
+            <a
+              className={`my-2 ${
+                isSocialHovered ? "opacity-100" : "opacity-0"
+              }`}
+              href="https://www.tiktok.com/@pacificpalaceee"
+            >
+              <FontAwesomeIcon
+                className="ease-in-out duration-100 mx-3 text-2xl hover:text-gray-900"
+                icon={faTiktok}
+              />
+            </a>
+            <a
+              className={`my-2 ${
+                isSocialHovered ? "opacity-100" : "opacity-0"
+              }`}
+              href="https://www.facebook.com/pacificpalaceee"
+            >
+              <FontAwesomeIcon
+                className="ease-in-out duration-100 mx-3 text-2xl hover:text-gray-900"
+                icon={faFacebook}
+              />
+            </a>
+          </div>
+        </div>
+        <div
+          className="flex items-center mx-3 relative"
+          onMouseEnter={() => setIsListenHovered(true)}
+          onMouseLeave={() => setIsListenHovered(false)}
+        >
+          <a className="cursor-pointer">Listen</a>
+
+          <div
+            className={`absolute flex flex-col top-10 left-6 social-links ${
+              isListenHovered ? "opacity-100" : "opacity-0"
+            }`}
+          >
+            <a
+              className={`my-2 ${
+                isListenHovered ? "opacity-100" : "opacity-0"
+              }`}
+              href="https://music.apple.com/us/artist/pacific-palace/1570737065"
+            >
+              <FontAwesomeIcon
+                className="ease-in-out duration-100 mx-3 text-2xl hover:text-gray-900"
+                icon={faItunesNote}
+              />
+            </a>
+            <a
+              className={`my-2 ${
+                isListenHovered ? "opacity-100" : "opacity-0"
+              }`}
+              href="https://open.spotify.com/artist/5rBKJhlJI8u3OFwglQGjcU?si=1G7RSXJfRtqAR-JUoClCeA&utm_medium=share&utm_source=linktree"
+            >
+              <FontAwesomeIcon
+                className="ease-in-out duration-100 mx-3 text-2xl hover:text-gray-900"
+                icon={faSpotify}
+              />
+            </a>
+          </div>
+        </div>
+      </nav>
+    </div>
   )
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,50 +1,74 @@
-import { useEffect } from "react"
+import React, { useEffect } from "react"
 
-const Home = () => {
+const Home: React.FC = () => {
   useEffect(() => {
-    const handleScroll = () => {
-      const tourElement = document.getElementById("tour") as HTMLElement
-      const videosElement = document.getElementById("videos") as HTMLElement
-      const tourImageElement = tourElement.querySelector(
-        ".cover-img"
-      ) as HTMLElement
-      const videosImageElement = videosElement.querySelector(
-        ".cover-img"
-      ) as HTMLElement
-
-      if (
-        !tourElement ||
-        !videosElement ||
-        !tourImageElement ||
-        !videosImageElement
-      )
-        return
-
-      const tourRect = tourElement.getBoundingClientRect()
-      const videosRect = videosElement.getBoundingClientRect()
-
-      const isTourVisible =
-        tourRect.top < window.innerHeight && tourRect.bottom >= 0
-      const isVideosVisible =
-        videosRect.top < window.innerHeight && videosRect.bottom >= 0
-
-      if (isTourVisible) {
-        const scrollPosition = window.scrollY
-        const zoomFactor = 1 + scrollPosition / 8000 // Adjust this factor as needed
-        tourImageElement.style.transform = `scale(${zoomFactor})`
-      } else {
-        tourImageElement.style.transform = "scale(1)"
-      }
-
-      if (isVideosVisible) {
-        const scrollPosition = window.scrollY
-        const zoomFactor = 1 + scrollPosition / 8000 // Adjust this factor as needed
-        videosImageElement.style.transform = `scale(${zoomFactor})`
-      } else {
-        videosImageElement.style.transform = "scale(1)"
+    const handleSmoothScroll = (event: Event) => {
+      event.preventDefault()
+      const targetId = (event.target as HTMLAnchorElement).getAttribute("href")
+      if (targetId) {
+        const targetElement = document.querySelector(targetId)
+        if (targetElement) {
+          targetElement.scrollIntoView({ behavior: "smooth" })
+        }
       }
     }
 
+    const anchorLinks = document.querySelectorAll("a[href^='#']")
+    anchorLinks.forEach((link) => {
+      link.addEventListener("click", handleSmoothScroll)
+    })
+
+    return () => {
+      anchorLinks.forEach((link) => {
+        link.removeEventListener("click", handleSmoothScroll)
+      })
+    }
+  }, [])
+
+  const handleScroll = () => {
+    const tourElement = document.getElementById("tour") as HTMLElement
+    const videosElement = document.getElementById("videos") as HTMLElement
+    const tourImageElement = tourElement.querySelector(
+      ".cover-img"
+    ) as HTMLElement
+    const videosImageElement = videosElement.querySelector(
+      ".cover-img"
+    ) as HTMLElement
+
+    if (
+      !tourElement ||
+      !videosElement ||
+      !tourImageElement ||
+      !videosImageElement
+    )
+      return
+
+    const tourRect = tourElement.getBoundingClientRect()
+    const videosRect = videosElement.getBoundingClientRect()
+
+    const isTourVisible =
+      tourRect.top < window.innerHeight && tourRect.bottom >= 0
+    const isVideosVisible =
+      videosRect.top < window.innerHeight && videosRect.bottom >= 0
+
+    if (isTourVisible) {
+      const scrollPosition = window.scrollY
+      const zoomFactor = 1 + scrollPosition / 8000 // Adjust this factor as needed
+      tourImageElement.style.transform = `scale(${zoomFactor})`
+    } else {
+      tourImageElement.style.transform = "scale(1)"
+    }
+
+    if (isVideosVisible) {
+      const scrollPosition = window.scrollY
+      const zoomFactor = 1 + scrollPosition / 8000 // Adjust this factor as needed
+      videosImageElement.style.transform = `scale(${zoomFactor})`
+    } else {
+      videosImageElement.style.transform = "scale(1)"
+    }
+  }
+
+  useEffect(() => {
     window.addEventListener("scroll", handleScroll)
     return () => {
       window.removeEventListener("scroll", handleScroll)
@@ -56,14 +80,37 @@ const Home = () => {
       <div id="cover" className="cover relative z-40">
         <img className="cover-img absolute" src="/img/cover.jpg" alt="Cover" />
       </div>
-      <div id="tour" className="cover scroll-section z-30">
+      <div
+        id="tour"
+        className="cover scroll-section z-30 flex flex-col items-center text-white font-arial"
+      >
+        <h2 className="z-50 my-10 text-5xl font-bold">TOUR</h2>
+
+        <p className="z-30 text-3xl">No upcoming shows currently</p>
+
         <img
           className="cover-img absolute min-w-full"
           src="/img/james_blue.png"
           alt="Cover"
         />
       </div>
-      <div id="videos" className="cover scroll-section relative z-20">
+      <div
+        id="videos"
+        className="cover scroll-section relative z-20 flex flex-col items-center"
+      >
+        <h2 className="z-50 my-10 text-5xl font-arial font-bold text-white">
+          VIDEOS
+        </h2>
+        <div className="z-50 my-10">
+          <iframe
+            width="1280"
+            height="720"
+            src="https://www.youtube.com/embed/GgaqsiYcpBQ?si=AYxVeU7iv-20wZ9P"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+            title="YouTube Video Player"
+          ></iframe>
+        </div>
         <img className="cover-img absolute" src="/img/hero.jpg" alt="Cover" />
       </div>
     </>
